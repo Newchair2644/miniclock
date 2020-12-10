672f45015e90d1f76d5852e4c732990551947c37
NumenHackerCracker <69362192+NumenHackerCracker@users.noreply.github.com>
2020-12-09 21:08:18 -0500

    Create miniclock.c

---
+++ miniclock.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+int h, m, s;
+int mode;
+	
+int main() {
+	printf("Select 12hr/24hr: ");
+	scanf("%d", &mode);
+	
+	if(mode != 12) {	//Checks whether mode is set to either 12 or 24 hour clock
+		if(mode != 24) {
+			printf("ERROR: Invalid clock\n");
+			return 0;
+		}
+	}
+	
+	printf("Set Time (%d hour clock): ", mode);  //Set time
+	scanf("%d %d %d", &h, &m, &s);
+	if(mode == 24 && h > 23) {		//Check hours (24)
+		printf("ERROR: Invalid Hour\n");
+		return 0;
+	}
+	if(mode == 12 && h > 12) {		//Check hours (12)
+		printf("ERROR: Invalid Hour\n");
+		return 0;
+	}
+	if(m > 59) {		//Check minutes
+		printf("ERROR: Invalid Minutes\n");
+		return 0;
+	}
+	if(s > 59) {		//Check seconds
+		printf("ERROR: Invalid seconds\n");
+		return 0;
+	}
+	
+	while(1) {
+		system("clear");		//Clears screen
+		printf("\n%02d:%02d:%02d\n", h, m, s);
+		fflush(stdout);		//Clear stdout buffer
+		s++;		//Increments seconds
+		if(s > 59) {		//Increments minutes; sets seconds to 0
+				m += 1;
+				s = 0;
+		}
+		if (m > 59) {		//Increments hours; sets min to 0
+				h += 1;
+				m = 0;
+		}
+		if (h > 12 && mode == 12) {		//Resets all (12 hr clock)
+				h = 1;
+				m = 0;
+				s = 0;
+		} else if(h > 23 && mode == 24) {		//Resets all (24 hour clock)
+				h = 0;
+				m = 0;
+				s = 0;
+		}
+		
+		sleep(1);		//1 second delay
+
+	}
+	return 0;
+}
