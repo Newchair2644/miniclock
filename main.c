6a18b6e860e027d6efd8811ee1ecfc9808953de9
NumenHackerCracker <69362192+NumenHackerCracker@users.noreply.github.com>
2021-01-19 12:52:45 -0500

    Update main.c
    my ocd

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main() {
 	printf("Select 12hr/24hr: ");
 	scanf("%d", &mode);
 	
-	if(mode != 12 && mode != 24) {	//Checks whether mode is set to either 12 or 24 hour clock
+	if (mode != 12 && mode != 24) {	//Checks whether mode is set to either 12 or 24 hour clock
 		printf("ERROR: Invalid clock\n");
 		return 0;
 	}
@@ -18,29 +18,29 @@ int main() {
 	printf("Separate hours, minutes, and seconds with spaces (12 00 00) = (h, m, s)\n");
 	printf("Set Time (%d hour clock): ", mode);  //Set time
 	scanf("%d %d %d", &h, &m, &s);
-	if(mode == 24 && h > 23) {		//Check hours (24)
+	if (mode == 24 && h > 23) {		//Check hours (24)
 		printf("ERROR: Invalid Hour\n");
 		return 0;
 	}
-	if(mode == 12 && h > 12) {		//Check hours (12)
+	if (mode == 12 && h > 12) {		//Check hours (12)
 		printf("ERROR: Invalid Hour\n");
 		return 0;
 	}
-	if(m > 59) {		//Check minutes
+	if (m > 59) {		//Check minutes
 		printf("ERROR: Invalid Minutes\n");
 		return 0;
 	}
-	if(s > 59) {		//Check seconds
+	if (s > 59) {		//Check seconds
 		printf("ERROR: Invalid seconds\n");
 		return 0;
 	}
 	
-	while(1) {
+	while (1) {
 		system("clear");		//Clears screen
 		printf("\n%02d:%02d:%02d\n", h, m, s);
 		fflush(stdout);		//Clear stdout buffer
 		s++;		//Increments seconds
-		if(s > 59) {		//Increments minutes; sets seconds to 0
+		if (s > 59) {		//Increments minutes; sets seconds to 0
 				m += 1;
 				s = 0;
 		}
@@ -52,7 +52,7 @@ int main() {
 				h = 1;
 				m = 0;
 				s = 0;
-		} else if(h > 23 && mode == 24) {		//Resets all (24 hour clock)
+		} else if (h > 23 && mode == 24) {		//Resets all (24 hour clock)
 				h = 0;
 				m = 0;
 				s = 0;
